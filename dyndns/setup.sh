cfd59052e827a6dcedd7660893ba61feb4ce194c
Kerbalnut <Kerbalnut@users.noreply.github.com>
2023-02-01 04:07:29 -0700

    Copied over changes to the loadcolors() function from test.sh (now rand.sh) to setup.sh

--- dyndns/setup.sh
+++ dyndns/setup.sh
@@ -192,6 +192,106 @@ loadcolors()
 	
 	# For more info see
 	#https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+	
+	# Background color:
+	
+	#echo -e "\033[38;5;208mpeach\033[0;00m"
+	# Taking apart this command: \033[38;5;208m
+	
+	# The \033 is the escape code. The [38; directs command to the foreground. If you want to change the background color instead, use [48; instead. The 5; is just a piece of the sequence that changes color. And the most important part, 208m, selects the actual color.
+	
+	RED_FOREG='\033[38;0;31m'    #'0;31' is Red's ANSI color code
+	GREEN_FOREG='\033[38;0;32m'  #'0;32' is Green's ANSI color code
+	YELLOW_FOREG='\033[38;1;32m' #'1;32' is Yellow's ANSI color code
+	BLUE_FOREG='\033[38;0;34m'   #'0;34' is Blue's ANSI color code
+	WHITE_FOREG='\033[38;1;37m'  #'1;37' is White's ANSI color code
+	BLACK_FOREG='\033[38;0;30m'  #'0;30' is Black's ANSI color code
+	DARKGRAY_FOREG='\033[38;1;30m'
+	LIGHTGRAY_FOREG='\033[38;0;37m'
+	NOCOLOR_FOREG='\033[38;0m'
+	
+	RED_BKG='\033[48;0;31m'    #'0;31' is Red's ANSI color code
+	GREEN_BKG='\033[48;0;32m'  #'0;32' is Green's ANSI color code
+	YELLOW_BKG='\033[48;1;32m' #'1;32' is Yellow's ANSI color code
+	BLUE_BKG='\033[48;0;34m'   #'0;34' is Blue's ANSI color code
+	WHITE_BKG='\033[48;1;37m'  #'1;37' is White's ANSI color code
+	BLACK_BKG='\033[48;0;30m'  #'0;30' is Black's ANSI color code
+	DARKGRAY_BKG='\033[48;1;30m'
+	LIGHTGRAY_BKG='\033[48;0;37m'
+	NOCOLOR_BKG='\033[48;0m'
+	
+	#https://opensource.com/article/19/9/linux-terminal-colors
+	
+	# Color 		Foreground 	Background
+	# Black 		\033[30m 	\033[40m
+	# Red 			\033[31m 	\033[41m
+	# Green 		\033[32m 	\033[42m
+	# Orange 		\033[33m 	\033[43m
+	# Blue 			\033[34m 	\033[44m
+	# Magenta 		\033[35m 	\033[45m
+	# Cyan 			\033[36m 	\033[46m
+	# Light gray 	\033[37m 	\033[47m
+	# default 		\033[39m 	\033[49m
+	
+	# There are some additional colors available for the background:
+	# Color 		Background
+	# Dark gray 	\033[100m
+	# Light red 	\033[101m
+	# Light green 	\033[102m
+	# Yellow 		\033[103m
+	# Light blue 	\033[104m
+	# Light purple 	\033[105m
+	# Teal 			\033[106m
+	# White 		\033[107m
+	
+	RED_FOREG='\033[31m'
+	GREEN_FOREG='\033[32m'
+	#YELLOW_FOREG='\033[38;1;32m' #'1;32' is Yellow's ANSI color code
+	BLUE_FOREG='\033[34m'
+	#WHITE_FOREG='\033[38;1;37m'  #'1;37' is White's ANSI color code
+	BLACK_FOREG='\033[30m'
+	#DARKGRAY_FOREG='\033[38;1;30m'
+	LIGHTGRAY_FOREG='\033[37m'
+	ORANGE_FOREG='\033[33m'
+	MAGENTA_FOREG='\033[35m'
+	CYAN_FOREG='\033[36m'
+	NOCOLOR_FOREG='\033[39m'
+	
+	RED_BKG='\033[41m'
+	GREEN_BKG='\033[42m'
+	YELLOW_BKG='\033[103m'
+	BLUE_BKG='\033[44m'
+	WHITE_BKG='\033[107m'
+	BLACK_BKG='\033[40m'
+	DARKGRAY_BKG='\033[100m'
+	LIGHTGRAY_BKG='\033[47m'
+	ORANGE_BKG='\033[43m'
+	MAGENTA_BKG='\033[45m'
+	CYAN_BKG='\033[46m'
+	NOCOLOR_BKG='\033[49m'
+	
+	LIGHT_RED_BKG='\033[101m'
+	LIGHT_GREEN_BKG='\033[102m'
+	LIGHT_BLUE_BKG='\033[104m'
+	LIGHT_PURPLE_BKG='\033[105m'
+	TEAL='\033[106m'
+	
+	#echo "Background color tests:"
+	#echo -e "I ${RED}love${NOCOLOR} Linux"
+	#printf "I ${RED}love${NOCOLOR} Linux\n"
+	#printf "I ${RED}love${NOCOLOR} Linux\n"
+	#printf "${RED_BKG}[CRITICAL ERROR]:${NOCOLOR_BKG}\n"
+	#printf "${RED_BKG}${BLACK_FOREG}[CRITICAL ERROR]:${NOCOLOR_FOREG}${NOCOLOR_BKG}\n"
+	#printf "${RED_BKG}${DARKGRAY_FOREG}[CRITICAL ERROR]:${NOCOLOR_FOREG}${NOCOLOR_BKG}\n"
+	#printf "${RED_BKG}${LIGHTGRAY_FOREG}[CRITICAL ERROR]:${NOCOLOR_FOREG}${NOCOLOR_BKG}\n"
+	#printf "${RED_BKG}${WHITE_FOREG}[CRITICAL ERROR]:${NOCOLOR_FOREG}${NOCOLOR_BKG}\n"
+	
+	##compgen -A variable | grep *_BKG
+	##echo (compgen -A variable | grep *_BKG)
+	#echo $(compgen -A variable | grep *_BKG)
+	#echo $((compgen -A variable | grep *_BKG))
+	#echo `compgen -A variable | grep *_BKG`
+	
 }
 
 # --------------------------------------------------------------------------------------------------------
